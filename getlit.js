c6ece11ce2025ef7e6a3a1031cd18dbda1084e15
fede <fede@lab-imac-96.music.fas.nyu.edu>
2017-11-07 15:52:54 -0500

    updates

--- getlit.js
+++ getlit.js
@@ -52,7 +52,7 @@ function getLit(x) {
       var equot = e.gsx$quickquote.$t;
       var epara = e.gsx$paraphrase.$t;
     //var nevent = "<h3>"+ekeyw+"</h3><p>"+equot+" ("+eauth+","+ebook+","+epage+")</p>";
-      var open = "<div id=\""+ekeyw.replace(/ /g,"_").toLowerCase();+"\"><h4>"+ekeyw+"</h4>"
+      var open = "<div id=\""+ekeyw.replace(/ /g,"_").toLowerCase()+"\"><h4>"+ekeyw+"</h4>"
       var intro = "<p>"+epara+"</p>";
       var nevent = "<blockquote>\""+equot+" \"("+eauth+", "+epage+")</blockquote>";
       var close = "</div>";
@@ -70,7 +70,7 @@ function getLit(x) {
 function makeMenu(x){
     x.append("<nav>");
     for (var k in keywords){
-      x.append("<a href=\"/#"+keywords[k].replace(/ /g,"_").toLowerCase();+"\">"+keywords[k]+" </a>");
+      x.append("<a href=\"/#"+keywords[k].replace(/ /g,"_").toLowerCase()+"\">"+keywords[k]+" </a>");
     }
     x.append("</nav>");
 }
