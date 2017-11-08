9fc113c5728ad66e8391f2c5dc5110b19553bdbc
Fede Camara Halac <camarafede@gmail.com>
2017-11-08 12:55:16 -0500

    updates

--- getlit.js
+++ getlit.js
@@ -52,7 +52,7 @@ function getLit(x) {
       var equot = e.gsx$quickquote.$t;
       var epara = e.gsx$paraphrase.$t;
     //var nevent = "<h3>"+ekeyw+"</h3><p>"+equot+" ("+eauth+","+ebook+","+epage+")</p>";
-      var open = "<div id=\""+ekeyw.replace(/ /g,"_").toLowerCase()+"\"><h4>"+ekeyw+"</h4>"
+      var open = "<div id=\""+ekeyw.replace(/ /g,"_").toLowerCase()+"\"><h4>"+ekeyw+"</h4>";
       //var intro = "<p>"+epara+"</p>";
       //var nevent = "<blockquote>\""+equot+" \"("+eauth+", "+epage+")</blockquote>";
       var nevent = "<p>"+epara+" ("+eauth+", "+epage+")</p>";
