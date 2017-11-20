8f093315a03455ec3f42d3f6025cc8e96e8b303c
Fede Camara Halac <camarafede@gmail.com>
2017-11-20 17:28:31 -0500

    updates

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ $(document).ready(function(x) {
   if ((w = $(window).width()) >= 600) w = w*0.5;
   h = $(window).height();
   $("body").append([titleData, containers]);
-  getLit($("#content"),https + spreadsheetID + 1 + altjson);
-  getBib($("#biblio"),https + spreadsheetID + 2 + altjson);
+  getLit($("#content"),https + spreadsheetID + "1" + altjson);
+  getBib($("#biblio"),https + spreadsheetID + "2" + altjson);
 });
