8fdf9f01fd69e601b60dfe85e8f7eb4d2b0c3ed4
fede <fede@lab-imac-96.music.fas.nyu.edu>
2017-11-21 11:58:42 -0500

    updates

--- config.js
+++ config.js
@@ -7,10 +7,13 @@ var https = "https://spreadsheets.google.com/feeds/list/";
 var altjson = "/public/values?alt=json";
 var w, h;
 var spreadsheetID = "1tMkdssQlN_wbGS1SjfORS7AOBspKvvun7_AvzxctMrE/";
+var formID = "1FAIpQLScijqg1IlmAWLsBgQ7jaUxEcCPUQqZroXG-9ZxXV4BvOKzbKg";
+var formURL = "https://docs.google.com/forms/d/e/"+formID+"/viewform";
 var titleData = "\
 <header>\
 <h1 onclick=\"location.href='"+ url + "'\">"+ title +"</h1>\
 <h2 onclick=\"location.href='" + url + "'\">" + subtitle + "</h2>\
+<h3>Go to live form <a href=\""+formURL+"\">here</a></h3>\
 </header>";
 var containers = "\
 <div id=menu></div>\
