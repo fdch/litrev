326c623f8a384f3f11170b2ad472740b8a9f772a
Fede Camara Halac <camarafede@gmail.com>
2018-04-16 18:32:32 +0200

    updates

--- fillout/getlit.js
+++ fillout/getlit.js
@@ -101,7 +101,7 @@ function getLit(x, sheet)
             <p>Click submit when ready:</p>\
           </td>\
           <td>\
-            <button type=\"submit\" onclick=\"pushForm("+uniqueForm+")\" >Submit</button>\
+            <button type=\"submit\" onclick=\"pushForm(\'"+uniqueForm+"\')\" >Submit</button>\
           </td>\
         </tr>\
       </tbody></table>\
