ba441fb5c19c649593c3c2d964b895828e2872c3
Fede Camara Halac <camarafede@gmail.com>
2018-04-22 13:44:55 -0400

    updates

--- fillout/main.js
+++ fillout/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(x) {
   if ((w = $(window).width()) >= 600) w = w*0.5;
   h = $(window).height();
-  $("body").append([titleData, containers]);
+  $("body").append(containers);
   getBib($("#biblio"), bib);
   getLit($("#content"), lit, currentForm, keys);
 });
