f27cbd4d82d8672f8f662a4c881ce26e4f8e7d75
Fede Camara Halac <camarafede@gmail.com>
2018-10-04 12:21:53 +0200

    look at mikey

--- js/abby_geti.js
+++ js/abby_geti.js
@@ -27,8 +27,8 @@ function getLit(x)
       var eprob = (e.gsx$probs.$t).split(' ');
       mikey=[];
       if (epara) {
-        for (var j=0; j<=eprob.length; j++) {
-          mikey.push(element('span', iKey[j] + ': ' + eprob[j]));
+        for (var j=0; j<=eprob.length-1; j++) {
+          mikey.push(element('span', iKey[j] + ': ' + eprob[j])+'. ');
         }
         x.appendChild(element('p', epara));
         for (let m in mikey) x.appendChild(mikey[m]);
