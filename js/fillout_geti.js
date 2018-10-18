2113a890f0956f45e19396e5fc48529de501d595
Fede Camara Halac <camarafede@gmail.com>
2018-10-18 09:42:23 +0200

    better console logs

--- js/fillout_geti.js
+++ js/fillout_geti.js
@@ -275,7 +275,7 @@ function getLit(x,y) {
 
     welcome();
     
-    console.log("| Waiting "+quoteTimeout/1000+" seconds for correct loading.     |");
+    console.log("| Waiting "+quoteTimeout/1000+" seconds for correct loading.        |");
     var countdown = setInterval(consoleLine, 1000);
     setTimeout(function() {    
       remainQuotes= alleqID.filter(f => !filQuoteID.includes(f));
