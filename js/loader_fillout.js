1f2170c33cf0ebcdc04dde874fce065b542fa3a4
Fede Camara Halac <camarafede@gmail.com>
2018-10-18 14:40:54 +0200

    TypeError: a is undefined

--- js/loader_fillout.js
+++ js/loader_fillout.js
@@ -136,7 +136,7 @@ function analyze(quote) {
   //  Sort the 'arr' object based on values and return its keys
   var arrSort = Object.keys(arr).sort(function(a,b){return arr[b]-arr[a]});
   //  Limit fetch up to 100 word candidates
-  var len     = (phar.length < maxQuery*10)?phar.length:maxQuery*20; 
+  var len     = (phar.length < maxQuery*10)?phar.length:maxQuery*10; 
   //  Loop through all candidates
   if (len) {
     for (var i=0; i<=len; i++) {
