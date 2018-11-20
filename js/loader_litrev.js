8321207fc80690d365c7b843ec04930c4c1f418c
Fede Camara Halac <camarafede@gmail.com>
2018-11-20 23:25:21 +0100

    format cites as \\cite{Xyz99}(p.99)

--- js/loader_litrev.js
+++ js/loader_litrev.js
@@ -42,9 +42,11 @@ function getLit() {
       var ek      = ekeyw.replace(/ /g,"_").toLowerCase();
       var eauth   = e.gsx$author.$t;
       var id      = eauth.slice(0,3)+(e.gsx$year.$t).slice(2);
+      
+// \cite{Man02} (p. 28)
       var quoteA  = anchor(
         "#"+id,
-        "["+eauth+" ("+id+"):"+e.gsx$page.$t+"]",
+        "\\cite{"+id+"} (p. "+e.gsx$page.$t+")",
         '',
         e.gsx$booktitle.$t+", "+eauth
       );
